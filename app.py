d712ea89ed171f5c3d172800e8cd7185652b93e7
Christopher Karanja Nganga <christopher.nganga@student.moringaschool.com>
2025-02-02 16:37:34 +0300

    Add database url

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from models import db
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'DATABASE_URL'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
